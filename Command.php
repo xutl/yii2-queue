be9691ab24bed797e6d4d93f715b3f2e570819c4
Peter <xutl@users.noreply.github.com>
2017-09-06 18:14:39 +0800

    Update Command.php

--- Command.php
+++ Command.php
@@ -1,1 +1,30 @@
 <?php
+namespace xutl\queue;
+
+use yii\queue\cli\Command as CliCommand;
+
+class Command extends CliCommand
+{
+    /**
+     * @var Queue
+     */
+    public $queue;
+  
+  /**
+     * Runs all jobs from beanstalk-queue.
+     * It can be used as cron job.
+     */
+    public function actionRun()
+    {
+        $this->queue->run();
+    }
+
+    /**
+     * Listens beanstalk-queue and runs new jobs.
+     * It can be used as demon process.
+     */
+    public function actionListen()
+    {
+        $this->queue->listen();
+    }
+}
